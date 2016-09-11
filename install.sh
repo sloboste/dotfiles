ce147fd9f8cf000c0f1bd9f4b3741efec5a930de
sloboste <sloboste@umich.edu>
2016-09-11 12:04:31 -0400

    Install fonts and dircolors necessary for Xresources.

--- install.sh
+++ install.sh
@@ -2,24 +2,37 @@
 
 set -e
 
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
+g_dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Setup powerline fonts
+echo "Installing Powerline fonts..."
+g_font_dir="${g_dotfiles_dir}/fonts"
+git clone https://github.com/powerline/fonts.git $g_font_dir
+bash $g_font_dir/install.sh
+
+# Setup solarized dircolors
+echo "Installing solarized dark dircolors..."
+g_dircolors_dir="${g_dotfiles_dir}/dircolors-solarized"
+git clone https://github.com/seebi/dircolors-solarized.git $g_dircolors_dir
+ln -s "${g_dircolors_dir}/dircolors.256dark" "${HOME}/.dircolors"
+
 g_xresources=".Xresources"
 g_xinitrc=".xinitrc"
 g_xserverrc=".xserverrc"
-g_inputrc=".inputrc"
 g_xmodmap=".Xmodmap"
+g_inputrc=".inputrc"
 
 g_config_files=($g_xresources $g_xinitrc $g_xserverrc $g_inputrc $g_xmodmap)
-
-# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
-g_dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Make symlinks for config files
 for filename in "${g_config_files[@]}"
 do
-    echo "Creating symlink for $filename"
+    echo "Installing $filename..."
     ln -s "${g_dotfiles_dir}/$filename" "${HOME}/$filename"
 done
 
 # Success!
+echo ""
 echo "Successfully installed!"
 echo "Restart X for changes to take effect."
